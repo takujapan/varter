abd75f9af5a9370f7fcd0b21feaaaa08cd6f49e2
Taku <takujapan1994@gmail.com>
2020-11-09 13:48:59 +0900

    静的ページ編集

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "静的ページ", type: :request do
+  describe "トップページ" do
+    it "正常なレスポンスを返すこと" do
+      get root_path
+      expect(response).to be_success
+      expect(response).to have_http_status "200"
+    end
+  end
+
+  describe "Varterとは？ページ" do
+    it "正常なレスポンスを返すこと" do
+      get about_path
+      expect(response).to be_success
+      expect(response).to have_http_status "200"
+    end
+  end
+end
