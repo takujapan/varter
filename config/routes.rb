3f924588016fcb405cdb97cbf83b6a83e565e129
Taku <takujapan1994@gmail.com>
2020-11-11 10:55:15 +0900

    ログインページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#home'
   get :about,     to: 'static_pages#about'
-  get :sign_up,   to: 'users#new'
+  get :signup,    to: 'users#new'
   get    :login,  to: 'sessions#new'
   post   :login,  to: 'sessions#create'
   delete :logout, to: 'sessions#destroy'
