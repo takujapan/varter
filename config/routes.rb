25471326573a7b8baa2d9d7c2f43b12eaecd7a67
Taku <takujapan1994@gmail.com>
2020-11-11 10:34:06 +0900

    ログイン機能のルーティングとアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
-  get :about,   to: 'static_pages#about'
-  get :sign_up, to: 'users#new'
+  get :about,     to: 'static_pages#about'
+  get :sign_up,   to: 'users#new'
+  get    :login,  to: 'sessions#new'
+  post   :login,  to: 'sessions#create'
+  delete :logout, to: 'sessions#destroy'
   resources :users
 end
