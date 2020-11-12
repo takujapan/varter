895eb935a6d3a945ae59d9b5127cbd1c6bc9040e
Taku <takujapan1994@gmail.com>
2020-11-12 20:50:56 +0900

    「ログインしたまま」にするチェックボックス追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,8 +12,6 @@ module SessionsHelper
 
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save :downcase_email
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
