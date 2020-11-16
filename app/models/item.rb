3592dc3957431b28e103e21c713856f67e3c8277
Taku <takujapan1994@gmail.com>
2020-11-16 11:55:05 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  validates :user_id, presence: true
-  validates :name, presence: true, length: { maximum: 30 }
-  validates :description, length: { maximum: 140 }
+  validates  :user_id, presence: true
+  validates  :name, presence: true, length: { maximum: 30 }
+  validates  :description, length: { maximum: 140 }
 end
