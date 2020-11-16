32055d7bcee723cbc3dd429634da3334f62ee6da
Taku <takujapan1994@gmail.com>
2020-11-16 15:56:35 +0900

    商品モデル作成に伴っての実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
-  validates  :user_id, presence: true
-  validates  :name, presence: true, length: { maximum: 30 }
-  validates  :description, length: { maximum: 140 }
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description, length: { maximum: 140 }
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,20 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Items.item }
-    description { "買ったけど、一回も使っていません" }
+    name { Faker::Name.name }
+    description { "昔買ったゲームです" }
     association :user
+    created_at { Time.current }
+  end
+
+  trait :yesterday do
+    created_at { 1.day.ago }
+  end
+
+  trait :one_week_ago do
+    created_at { 1.week.ago }
+  end
+
+  trait :one_month_ago do
+    created_at { 1.month.ago }
   end
 end
