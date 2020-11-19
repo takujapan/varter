337159ee85f75361192d97bd198def620615b604
Taku <takujapan1994@gmail.com>
2020-11-20 08:50:06 +0900

    logged_in_userのテスト

---
+++ spec/requests/favorites_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "お気に入り登録機能", type: :request do
+  let(:user) { create(:user) }
+  let(:item) { create(:item) }
+
+  context "お気に入り登録処理" do
+    context "ログインしていない場合" do
+      it "お気に入り登録はできず、ログインページへリダイレクトすること" do
+        expect {
+          post "/favorites/#{item.id}/create"
+        }.not_to change(Favorite, :count)
+        expect(response).to redirect_to login_path
+      end
+
+      it "お気に入り解除はできず、ログインページへリダイレクトすること" do
+        expect {
+          delete "/favorites/#{item.id}/destroy"
+        }.not_to change(Favorite, :count)
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
+end
