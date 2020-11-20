111a2e520b1beb7755c2f32ecf5b668948d73d1e
Taku <takujapan1994@gmail.com>
2020-11-20 10:12:29 +0900

    お気に入り登録のテスト作成

--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -5,6 +5,38 @@ RSpec.describe "お気に入り登録機能", type: :request do
   let(:item) { create(:item) }
 
   context "お気に入り登録処理" do
+    context "ログインしている場合" do
+      before do
+        login_for_request(user)
+      end
+
+      it "商品のお気に入り登録ができること" do
+        expect {
+          post "/favorites/#{item.id}/create"
+        }.to change(user.favorites, :count).by(1)
+      end
+
+      it "商品のAjaxによるお気に入り登録ができること" do
+        expect {
+          post "/favorites/#{item.id}/create", xhr: true
+        }.to change(user.favorites, :count).by(1)
+      end
+
+      it "商品のお気に入り解除ができること" do
+        user.favorite(item)
+        expect {
+          delete "/favorites/#{item.id}/destroy"
+        }.to change(user.favorites, :count).by(-1)
+      end
+
+      it "商品のAjaxによるお気に入り解除ができること" do
+        user.favorite(item)
+        expect {
+          delete "/favorites/#{item.id}/destroy", xhr: true
+        }.to change(user.favorites, :count).by(-1)
+      end
+    end
+
     context "ログインしていない場合" do
       it "お気に入り登録はできず、ログインページへリダイレクトすること" do
         expect {
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Users", type: :system do
   let(:user) { create(:user) }
   let(:admin_user) { create(:user, :admin) }
   let!(:other_user) { create(:user) }
+  let!(:item) { create(:item, user: user) }
 
   describe "ユーザー一覧ページ" do
     context "管理者ユーザーの場合" do
@@ -182,6 +183,42 @@ RSpec.describe "Users", type: :system do
         expect(user.favorite?(item)).to be_truthy
         user.unfavorite(item)
         expect(user.favorite?(item)).to be_falsey
+      end
+
+      it "トップページからお気に入り登録/解除ができること", js: true do
+        visit root_path
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{item.id}/create"
+        link.click
+        link = find('.unlike')
+        expect(link[:href]).to include "/favorites/#{item.id}/destroy"
+        link.click
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{item.id}/create"
+      end
+
+      it "ユーザー個別ページからお気に入り登録/解除ができること", js: true do
+        visit user_path(user)
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{item.id}/create"
+        link.click
+        link = find('.unlike')
+        expect(link[:href]).to include "/favorites/#{item.id}/destroy"
+        link.click
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{item.id}/create"
+      end
+
+      it "商品個別ページからお気に入り登録/解除ができること", js: true do
+        visit item_path(item)
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{item.id}/create"
+        link.click
+        link = find('.unlike')
+        expect(link[:href]).to include "/favorites/#{item.id}/destroy"
+        link.click
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{item.id}/create"
       end
     end
   end
