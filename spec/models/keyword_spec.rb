df5c689fc549aff1365c5b9ef52d77c369a7202e
Taku <takujapan1994@gmail.com>
2020-11-24 09:01:49 +0900

    Keywordのモデルテスト

--- spec/models/keyword_spec.rb
+++ spec/models/keyword_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Keyword, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:keyword) { create(:keyword) }
+
+  it "有効な状態であること" do
+    expect(keyword).to be_valid
+  end
 end
